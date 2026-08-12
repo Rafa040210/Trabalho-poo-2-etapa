a64837ec896aa5764bd0b133c9335dd4503571bc
Ariely <arielysantos156@gmail.com>
2026-08-12 17:21:29 -0300

    main inicial

---
+++ main.py
@@ -0,0 +1,21 @@
+from datetime
+
+print("Sistema Educacional de Escola de Hangar")#colocar cor
+
+turma1_exemplo = Turma()
+turma2_exemplo = Turma()
+
+prof1_exemplo = Professor()
+prof2_exemplo = Professor()
+
+aluno1_0_exemplo = Aluno()
+aluno1_1_exemplo = Aluno()
+aluno1_2_exemplo = Aluno()
+aluno1_3_exemplo = Aluno()
+aluno1_4_exemplo = Aluno()
+
+aluno2_0_exemplo = Aluno()
+aluno2_1_exemplo = Aluno()
+aluno2_2_exemplo = Aluno()
+aluno2_3_exemplo = Aluno()
+aluno2_4_exemplo = Aluno()
